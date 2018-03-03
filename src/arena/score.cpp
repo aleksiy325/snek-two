3195832f9f0711094875b783f9348cb4feefc51b
Aleksiy Kudelsky <akudelsky@kinsolresearch.com>
2018-03-03 05:20:35 -0800

    ready for train

--- src/arena/score.cpp
+++ src/arena/score.cpp
@@ -3,28 +3,25 @@
 #include <iostream>
 
 
-class Score{
-	int ticks;
-  int rank;
+class Score {
+    int ticks;
+    int rank;
 public:
-	Score();
-	Score(int ticks, int rank);
-	double evalFitness();
+    Score();
+    Score(int ticks, int rank);
+    double evalFitness();
 };
 
-Score::Score(){
-	ticks = 0;
+Score::Score() {
+    ticks = 0;
 }
 
-Score::Score(int ticks, int rank){
-	this->ticks = ticks;
-  this->rank = rank;
-  // std::cout << "rank = " << rank << "\n";
-  // std::cout << "ticks = " << ticks<< "\n";
+Score::Score(int ticks, int rank) {
+    this->ticks = ticks;
+    this->rank = rank;
 }
 
-double Score::evalFitness(){
-	double fitness = (300.0/(rank+1.0)) + rank / 100.0;
-  // std::cout << "fitness is " << fitness << "\n";
-	return fitness;
+double Score::evalFitness() {
+    double fitness = (300.0 / (rank + 1.0)) + ticks  / 100.0;
+    return fitness;
 }
